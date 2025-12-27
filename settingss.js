5ce1d7d73b29d2abcb634f4894166d490aac7016
jonathanxingyun-afk <jonathanxingyun@gmail.com>
2025-12-27 06:24:23 +0300

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,7 +1,7 @@
 
 module.exports = {
   SESSION_ID: "",  // add your Session Id here
-  OWNER_NUMBER: "", // put your phone number here
+  OWNER_NUMBER: "0758572890", // put your phone number here
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   TIMEZONE: "Africa/Kampala" //put your country timeZone....leave blank if u don't know.
 };
